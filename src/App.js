aa964380023b1a4979313ce402c2e6bfb9514b6c
Ajay <majay1638@gmail.com>
2019-11-12 11:54:31 +0530

    added Palette

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Palette from './Palette';
+import SeedColor from './SeedColor';
+import './styles/palette.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Palette {...SeedColor[5]} />
+
     </div>
   );
 }
