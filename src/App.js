d586a5cf68a69f4ef024ab2bfe86a0cd18e69286
Ajay <majay1638@gmail.com>
2019-11-13 07:39:48 +0530

    Added helperColor method

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Palette from './component/palette/palette.component';
-import SeedColor from './SeedColor';
+import SeedColors from './SeedColor';
+import { generatePalette } from './colorHelper';
 
-function App() {
-  return (
-    <div>
-      <Palette {...SeedColor[2]} />
+class App extends Component {
+  render() {
+    console.log(generatePalette(SeedColors[4]))
+    return (
 
-    </div>
-  );
+
+
+      <div>
+        <Palette {...SeedColors[6]} />
+
+      </div>
+    );
+
+
+  }
 }
 
 export default App;
